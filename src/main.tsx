27df2f7da61a7ba44497feacc3e081bb0f491b10
stargiosoft <stargiosoft@gmail.com>
2025-12-29 12:23:47 +0900

    refactor: separate Sentry config to root folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,7 @@
-import * as Sentry from "@sentry/react";
+import "../sentry.config";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
-// Sentry 초기화
-Sentry.init({
-  dsn: "https://39080e19aa10174928f35f1cfd924d45@o4510615959109632.ingest.us.sentry.io/4510615989059584",
-  sendDefaultPii: true,
-  // 환경 구분 (Vercel 환경변수 사용)
-  environment: import.meta.env.MODE,
-});
 
 createRoot(document.getElementById("root")!).render(<App />);
   
